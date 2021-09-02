1bd0881a77ac6143307beb7f76af122a9c93b5d5
@_Victor_ <duartedvictor@gmail.com>
2021-09-02 15:12:56 -0400

    modificaciones fianles

--- cls.js/ja.js
+++
@@ -1,3 +0,0 @@
-const num = (num1,num2) =>{
- 
-}
---
+++ index.js
@@ -0,0 +1,2 @@
+window.onload=function() {alert ("Bienvenido a la biblioteca Omar Barrientos");}
+window.onunload=function(){alert("Vuelva cuando quiera digale a su amistades sobre la biblioteca");}
